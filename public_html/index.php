8e61a8d3feed7b3ec34d1f06ac19599af42ac0cd
桾　知香子 <>
2021-04-11 19:43:36 +0900

    テスト

---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<?php
+
+	$string = file_get_contents("sample.html");
+	
+	$pattern = '/<img.*?">/';
+	
+	preg_match_all($pattern, $string , $match);
+	
+foreach($match[0] as $value){
+print "$value";} 
