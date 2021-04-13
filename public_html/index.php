97a1c866d48b9ac3ba69c0f313d76f0e2682d620
桾　知香子 <>
2021-04-13 10:06:40 +0900

    画像の正規表現

--- public_html/index.php
+++ public_html/index.php
@@ -5,8 +5,9 @@
 	<title>Document</title>
 </head>
 <body>
-<!--
-	<　?php
+
+	
+	<?php
 
 	$string = file_get_contents("sample.html");
 	
@@ -18,41 +19,6 @@ foreach($match[0] as $value){
 print "$value";} 
 
 ?>
--->
-<style>.f-container{
-	display: -webkit-flex;
-	display: -moz-flex;
-	display: -ms-flex;
-	display: -o-flex;
-	display: flex;
-	justify-content: flex-start;
-	-webkit-flex-wrap: wrap;
-	-moz-flex-wrap: wrap;
-	-ms-flex-wrap: wrap;
-	-o-flex-wrap: wrap;
-	flex-wrap: wrap;
-	}
-	
-	.f-item {
-		width: calc(100% / 3);
-		height: 500px;
-		background-color: hotpink;
-		border: solid 10px #fff;
-		box-sizing: border-box;
-		color: aqua;
-		text-align: center;
-		line-height: 500px;
-	}</style>
-<div class="f-container">
-	<div class="f-item">
-		<p>いい感じ</p>
-	</div>
-	<div class="f-item">
-		<p>いい感じ</p>
-	</div>
-	<div class="f-item">
-		<p>いい感じ</p>
-	</div>
-</div>
+
 </body>
 </html>
